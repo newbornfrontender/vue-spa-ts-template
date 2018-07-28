e4c59f62b536c304ef9ebbe8f8e8a540e98906ff
Infant Frontender <newbornfrontender@gmail.com>
2018-07-28 09:52:51 +0300

    Исправил путь у dts файла

--- build/parts/mode.ts
+++ build/parts/mode.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { Mode } from '../definitions/Mode.d';
+import { Mode } from '../definitions/parts/Mode';
 
 const config: Mode = {
   mode: process.env.NODE_ENV || 'development',
