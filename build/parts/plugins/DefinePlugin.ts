59395cc89f8cb71cbcb24f5bbbd9029e096d97f7
Infant Frontender <newbornfrontender@gmail.com>
2018-07-28 17:33:08 +0300

    Добавил разделители, убрал лишние префиксы

--- build/parts/plugins/DefinePlugin.ts
+++ build/parts/plugins/DefinePlugin.ts
@@ -1,23 +1,29 @@
 'use strict';
 
-declare const process: {
-  env: {
-    NODE_ENV: string;
-    NODE_CUSTOM_ENV: string;
-  };
-};
+// -----------------------------------------------------------------------------
 
 import { Configuration, DefinePlugin } from 'webpack';
 
-const config: Configuration = {
+// -----------------------------------------------------------------------------
+
+declare const process: {
+  env: {
+    NODE_ENV: string;
+  };
+};
+
+// -----------------------------------------------------------------------------
+
+const partConfig: Configuration = {
   plugins: [
     new DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        'NODE_CUSTOM_ENV': JSON.stringify(process.env.NODE_CUSTOM_ENV),
       },
     }),
   ],
 };
 
-export default config;
+// -----------------------------------------------------------------------------
+
+export default partConfig;
--- build/parts/plugins/EnvironmentPlugin.ts
+++ build/parts/plugins/EnvironmentPlugin.ts
@@ -1,21 +1,27 @@
 'use strict';
 
-declare const process: {
-  env: {
-    NODE_ENV: string;
-    NODE_CUSTOM_ENV: string;
-  };
-};
+// -----------------------------------------------------------------------------
 
 import { Configuration, EnvironmentPlugin } from 'webpack';
 
-const config: Configuration = {
+// -----------------------------------------------------------------------------
+
+declare const process: {
+  env: {
+    NODE_ENV: string;
+  };
+};
+
+// -----------------------------------------------------------------------------
+
+const partConfig: Configuration = {
   plugins: [
     new EnvironmentPlugin([
       'NODE_ENV',
-      'NODE_CUSTOM_ENV',
     ]),
   ],
 };
 
-export default config;
+// -----------------------------------------------------------------------------
+
+export default partConfig;
