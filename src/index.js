d556c1ebb031c9ad667003e17e205c731fa0aa5d
Infant Frontender <newbornfrontender@gmail.com>
2018-07-28 09:54:52 +0300

    Добавил логи для просмотра переменных сред

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 'use strict';
 
-console.log(process.env.NODE_CUSTOM_ENV);
+console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
+console.log(`process.env.NODE_CUSTOM_ENV = ${process.env.NODE_CUSTOM_ENV}`);
