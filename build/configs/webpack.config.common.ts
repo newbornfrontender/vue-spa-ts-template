c27c02c753e9ba94cce562fe55de8860266d9483
Infant Frontender <newbornfrontender@gmail.com>
2018-07-28 09:56:54 +0300

    Добавил DefinePlugin по умолчанию

--- build/configs/webpack.config.common.ts
+++ build/configs/webpack.config.common.ts
@@ -7,9 +7,13 @@ import WebpackMerge from 'webpack-merge';
 import mode from '../parts/mode';
 import entry from '../parts/entry';
 
+// import EnvironmentPlugin from '../parts/plugins/EnvironmentPlugin';
+import DefinePlugin from '../parts/plugins/DefinePlugin';
 import WebpackDotenvPlugin from '../parts/plugins/WebpackDotenvPlugin';
 
 const plugins: Configuration = WebpackMerge(
+  // EnvironmentPlugin,
+  DefinePlugin,
   WebpackDotenvPlugin,
 );
 
