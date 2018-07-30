665a0d37cbba677561edb09c75030dce5e2b8f85
Infant Frontender <newbornfrontender@gmail.com>
2018-07-30 16:21:18 +0300

    Переименовал расширение файла в ".ts"

--- src/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-import grid from './grid.css';
-import styles from './styles.css';
-
-console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
---
+++ src/index.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+// =============================================================================
+
+// Articles describing how to import styles into TS files
+
+// EN: https://medium.com/@sapegin/css-modules-with-typescript-and-webpack-6b221ebe5f10
+// RU: http://qaru.site/questions/843718/how-to-import-css-modules-with-typescript-react-and-webpack
+
+// =============================================================================
+
+// import styles from './styles.css';
+// import './styles.css';
+// var styles = require('./styles.css');
+// require('./styles.css');
+
+console.log(`process.env.NODE_ENV = ${process.env.NODE_ENV}`);
