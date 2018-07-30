6c5a5507be982068c718f01c7f11af87e9dcf81a
Infant Frontender <newbornfrontender@gmail.com>
2018-07-30 11:44:12 +0300

    Добавил утилиту с конфигурациями под рабочие среды

---
+++ build/modules/utils/process/env.ts
@@ -0,0 +1,33 @@
+'use strict';
+
+const getModeValue = (value: string): 'development' | 'production' | 'none' => {
+  switch (value) {
+    case 'development' || 'test': return 'development';
+    case 'production': return value;
+    case 'none': return value;
+    default: 'development';
+  };
+};
+
+const getHashDigestValue = (value: string): 'hex' | 'latin1' | 'base64' => {
+  switch (value) {
+    case 'hex': return value;
+    case 'latin1': return value;
+    case 'base64': return value;
+    default: 'hex';
+  };
+};
+
+const getHashDigestLengthValue = (value: string): number => {
+  if (Number(value) >= 1) {
+    return Number(value);
+  };
+
+  return 10;
+};
+
+export {
+  getModeValue as modeValue,
+  getHashDigestValue as hashDigestValue,
+  getHashDigestLengthValue as hashDigestLengthValue,
+};
