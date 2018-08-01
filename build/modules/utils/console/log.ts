86ef66403da8740570a554a38690f905e345dd90
Infant Frontender <newbornfrontender@gmail.com>
2018-08-01 20:21:34 +0300

    Добавил лог для типа сборки

--- build/modules/utils/console/log.ts
+++ build/modules/utils/console/log.ts
@@ -2,31 +2,35 @@
 
 import process from '../../../definitions/process';
 
-const getHashDigestLog = ():void => {
-  if (
-    process.env.NODE_ENV === 'production' &&
-    process.env.HASH_DIGEST !== 'hex' && 'latin1' && 'base64'
-  ) {
-    return console.log(
-      'Please set the "HASH_DIGEST" in ".env/build/production" to "hex", ' +
-      '"latin1" or "base64"! Now "HASH_DIGEST" using "hex" value.'
-    );
+const getModeLog = (): void => {
+  switch (process.env.NODE_ENV) {
+    case 'development': return console.log('development');
+    case 'production': return console.log('production');
+    case 'none': return console.log('none');
+    default: console.log('development');
   };
 };
 
-const getHashDigestLengthLog = ():void => {
+const getHashDigestLog = (): void => {
   if (
-    process.env.NODE_ENV === 'production' &&
-    Number(process.env.HASH_DIGEST_LENGTH) <= 0
+    (process.env.NODE_ENV === 'production') &&
+    (process.env.HASH_DIGEST !== 'hex' && 'latin1' && 'base64')
   ) {
-    return console.log(
-      '"HASH_DIGEST_LENGTH" in ".env/build/production" are undefined! ' +
-      'Now "HASH_DIGEST_LENGTH" using "10" value.'
-    );
+    return console.log('getHashDigestLog');
+  };
+};
+
+const getHashDigestLengthLog = (): void => {
+  if (
+    (process.env.NODE_ENV === 'production') &&
+    (Number(process.env.HASH_DIGEST_LENGTH) <= 0)
+  ) {
+    return console.log('getHashDigestLengthLog');
   };
 };
 
 export {
+  getModeLog as modeLog,
   getHashDigestLog as hashDigestLog,
   getHashDigestLengthLog as hashDigestLengthLog,
 };
