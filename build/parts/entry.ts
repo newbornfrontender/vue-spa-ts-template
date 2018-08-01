4d4d8ce2ec052c777890f36b5a1fb308ad33aa1e
Infant Frontender <newbornfrontender@gmail.com>
2018-08-01 20:22:10 +0300

    Изменил путь у index

--- build/parts/entry.ts
+++ build/parts/entry.ts
@@ -3,14 +3,13 @@
 // -----------------------------------------------------------------------------
 
 import { Configuration } from 'webpack';
-
 import { resolve } from 'path';
 
 // -----------------------------------------------------------------------------
 
 const partConfig: Configuration = {
   entry: {
-    index: resolve(__dirname, '../../src/index.ts'),
+    index: resolve(__dirname, '../..', 'src/index.ts'),
   },
 };
 
